73d917bf6c1659240d3ec3bea5262b9b0a0efc20
Michaël CHLON <mic.a.elle.chlon@gmail.com>
2024-04-18 09:45:49 +0200

    💫 feat: Switch to Standalone Directive

--- src/app/welcome/details/details.component.ts
+++ src/app/welcome/details/details.component.ts
@@ -1,16 +1,16 @@
 import { Component } from "@angular/core";
 import { AnalyticsService } from "src/app/shared/analytics.service";
-import { SharedModule } from "src/app/shared/shared.module";
+import { HighlightDirective } from "src/app/shared/highlight.directive";
 
 @Component({
   standalone: true,
-  imports: [SharedModule],
+  imports: [HighlightDirective],
   selector: "app-details",
   templateUrl: "./details.component.html",
   styleUrls: ["./details.component.css"],
 })
 export class DetailsComponent {
-  constructor(private analyticsService: AnalyticsService) { }
+  constructor(private analyticsService: AnalyticsService) {}
 
   onClick() {
     this.analyticsService.registerClick();
